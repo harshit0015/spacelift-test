18f60c5e57b756627b9300e2961dad30e21af8d8
harshit0015 <76742193+harshit0015@users.noreply.github.com>
2023-02-27 13:17:46 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>2.0"
     }
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "~> 0.1.11"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
